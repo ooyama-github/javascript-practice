5428c02cb392b9550e3143dfd70b87a4c489b5fa
ooyama-github <117147451+ooyama-github@users.noreply.github.com>
2023-02-20 05:23:07 +0000

    2章終了

--- app.js
+++ app.js
@@ -1,2 +1,55 @@
 // アラートを表示させる記述
-alert('Hello World');
+// let hello = 'Hello World';
+// alert(hello);
+
+
+// みかんとリンゴの条件分岐
+// let orange = 100;
+// let apple = 120;
+
+// if(orange < apple){
+//     alert('みかんの値段がりんごより安い');
+// } else if(orange == apple){
+//     alert('みかんとりんごが同じ値段');
+// } else{
+//     alert('みかんの値段がリンゴより高い');
+// }
+
+
+// while文で100を超えるまで2を掛け続ける処理文
+// let max = 100;
+// let num = 1;
+// let count = 0;
+
+// while(num < max){
+//     num = num * 2;
+//     count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+
+// for文の使い方
+// for ( 初期値; 条件式; 増減値 ){
+//   繰り返し処理
+// }
+
+// let i;
+// let num = 0;
+
+// for(i=1; i<11; i++){
+//     num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
+
+
+// 足し算
+alert(3 + 3);
+// 引き算
+alert(2 - 2);
+// 掛け算
+alert(4 * 2);
+// 割り算
+alert(6 / 2);
